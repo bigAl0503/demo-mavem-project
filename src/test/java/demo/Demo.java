1350a2fc0ae65767efbde571848476f1e116b98e
Oleksii K <oleksii0503@aol.com>
2024-02-10 11:38:31 -0500

    Initial commit

---
+++ src/test/java/demo/Demo.java
@@ -0,0 +1,21 @@
+package demo;
+
+import com.github.javafaker.Faker;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class Demo {
+
+
+    public static void main(String[] args) {
+
+       // WebDriver driver = new ChromeDriver();
+
+        Faker faker = new Faker();
+
+        System.out.println(faker.business().creditCardNumber());
+        System.out.println(faker.chuckNorris().fact());
+
+    }
+}
